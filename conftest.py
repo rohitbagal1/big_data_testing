939fa210e65c9710584497a078a3ce1e6b38a318
rohitbagal1 <90562891+rohitbagal1@users.noreply.github.com>
2024-09-19 16:56:22 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,35 +1,54 @@
+from py.xml import html
 import pytest
-
-# Specify the path and file name for the HTML report
-@pytest.mark.hookwrapper
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    rep = outcome.get_result()
-    setattr(item, "rep_" + rep.when, rep)
-
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
-    cells.pop()
-
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(report.duration, class_='col-time'))
-    cells.pop()
+from UtilityScripts.update_parameter_json import update_json_parameters
 
 def pytest_html_report_title(report):
-    report.title = "Pytest Automation Report"
+    report.title = "API Automation"
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("general")
+    group._addoption(
+        "-S",
+        dest="Server",
+        default="reqres.in",
+        help="this is our api server",
+    )
+
 
 def pytest_configure(config):
-    config._metadata['Project Name'] = 'Your Project Name'
-    config._metadata['Tester'] = 'Your Name'
+    config._metadata["Website URL"] = "https://reqres.in"
+    if "-S" in config.invocation_params.args:
+        server = config.getoption("-S")
+        update_json_parameters("server", server)
 
-# Generate HTML report with timestamp
-@pytest.mark.optionalhook
-def pytest_html_results_summary(prefix, summary, postfix):
-    prefix.extend([html.p("Generated at: {0}".format(datetime.now()) )])
 
-# Specify the path and file name for the HTML report
-def pytest_html_results_table_html(report, data):
-    if report.passed:
-        del data[:]
+
+
+def pytest_html_results_summary(prefix):
+    prefix.extend([html.p("This demo framework is created by Rohit Bagal")])
+
+
+def pytest_html_results_table_header(cells):
+    cells.insert(1, html.th("Description"))
+    cells.pop(2)
+    cells.pop()  # to remove last column name
+
+
+
+def pytest_html_results_table_row(report, cells):
+    cells.insert(1, html.td(report.description))
+    cells.pop(2)
+    cells.pop()  # to remove last row values
+
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    try:
+        report.description = str(item.function.__doc__) + " " + str((list(item.callspec.params.items())[0])[1])
+    except:
+        report.description = str(item.function.__doc__)
+    print(report.description)
