d6a0fa3343be2460f24ffd297767a148c3ebc109
rohitbagal1 <90562891+rohitbagal1@users.noreply.github.com>
2024-10-01 11:21:01 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -12,17 +12,18 @@ def pytest_runtest_makereport(item, call):
         report.description = str(item.function.__doc__)
     print(report.description)
 
-# Customize the HTML report table header
-def pytest_html_results_table_header(cells):
-    cells.insert(2, '<th>Description</th>')
-    cells.insert(1, '<th class="sortable time" col="time">Time</th>')
-    cells.pop()
 
 # Customize the HTML report table row
+def pytest_html_results_table_header(cells):
+    cells.insert(1, html.th("Description"))
+    cells.pop(2)
+    cells.pop()  # to remove last column name
+
 def pytest_html_results_table_row(report, cells):
-    cells.insert(2, f'<td>{getattr(report, "description", "")}</td>')
-    cells.insert(1, f'<td class="col-time">{getattr(report, "duration", 0)}</td>')
-    cells.pop()
+    cells.insert(1, html.td(report.description))
+    cells.pop(2)
+    cells.pop()  # to remove last row values
+
 
 # Set the report title
 def pytest_html_report_title(report):
