a615216db70495f897eb78cef8a8052ec4bd6c1f
rohitbagal1 <90562891+rohitbagal1@users.noreply.github.com>
2024-10-01 10:04:41 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -8,32 +8,28 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
     setattr(item, "rep_" + rep.when, rep)
 
+# Customize the HTML report table header
 def pytest_html_results_table_header(cells):
     cells.insert(2, '<th>Description</th>')
     cells.insert(1, '<th class="sortable time" col="time">Time</th>')
     cells.pop()
 
+# Customize the HTML report table row
 def pytest_html_results_table_row(report, cells):
     cells.insert(2, f'<td>{getattr(report, "description", "")}</td>')
     cells.insert(1, f'<td class="col-time">{getattr(report, "duration", 0)}</td>')
     cells.pop()
 
+# Set the report title
 def pytest_html_report_title(report):
     report.title = "Pytest Automation Report"
 
-@pytest.hookimpl(tryfirst=True)
+# Add metadata to the report
 def pytest_configure(config):
-    if config.pluginmanager.hasplugin("html"):
-        config._metadata = getattr(config, '_metadata', {})
-        config._metadata['Project Name'] = 'Your Project Name'
-        config._metadata['Tester'] = 'Your Name'
-    else:
-        print("pytest-html plugin is not available. Metadata will not be added.")
+    config._metadata['Project Name'] = 'Your Project Name'
+    config._metadata['Tester'] = 'Your Name'
 
+# Add a summary message to the report
 @pytest.mark.optionalhook
 def pytest_html_results_summary(prefix, summary, postfix):
     prefix.extend([f"<p>Generated at: {datetime.now()}</p>"])
-
-def pytest_html_results_table_html(report, data):
-    if report.passed:
-        del data[:]
