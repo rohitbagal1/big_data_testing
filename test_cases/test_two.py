4c46249960affaf785fff8c103ad684b055be10e
rohitbagal1 <rdb.vicky@gmail.com>
2025-01-02 10:24:35 +0530

    new code

--- test_cases/test_two.py
+++ test_cases/test_two.py
@@ -1,9 +1,10 @@
 import pytest
 from transformation.transformation_one import TransFormClass
 
-class TestMathOperations(TransFormClass):
+class TestMathOperations: 
     def test_product_one(self):
-        status = self.execute_transformations()
+        transformer = TransFormClass() 
+        status = transformer.execute_transformations() 
         assert status == True, "Status is False hence test case failed"
 
     def test_product_two(self):
