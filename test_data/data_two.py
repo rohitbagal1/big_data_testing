557c72d5992bdf98054ede28aedd46b815177560
rohitbagal1 <rdb.vicky@gmail.com>
2025-01-02 13:50:46 +0530

    commit

--- test_data/data_two.py
+++ test_data/data_two.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
+from pyspark.sql.functions import Row
 spark = SparkSession.builder.appName("YourAppName").getOrCreate()
 data1 = [(1, "Alice","Purnia","12/06/2003","23/09/2050",-1),(2, "Bob","Jaipur","12/06/2033","23/09/2055",0),(3, "Charlie","Delhi","12/06/2053","23/09/2055",1)]    
 columns1 = ["id", "name","Address","dob","dob1","status"]
