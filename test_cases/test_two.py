dc4618018f21e74739c45439fc5f8d6143e94cbf
rohitbagal1 <rdb.vicky@gmail.com>
2025-01-02 10:13:21 +0530

    commit new code

--- test_cases/test_two.py
+++ test_cases/test_two.py
@@ -1,16 +1,10 @@
 import pytest
 from transformation.transformation_one import TransFormClass
-class TestMathOperations:
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.transformer = TransFormClass()
-    
+
+class TestMathOperations(TransFormClass):
     def test_product_one(self):
-        status = self.transformer.execute_transformations()
-        assert status==True, "Status is False hence test case failed"
+        status = self.execute_transformations()
+        assert status == True, "Status is False hence test case failed"
 
     def test_product_two(self):
         assert 5 - 3 == 2, "Subtraction test failed"
-
-
-
