e9f259f4123f3b4bd7528ad7ac71f48c41e8e7ce
rohitbagal1 <rdb.vicky@gmail.com>
2025-01-02 10:33:25 +0530

    all test class changed

--- test_cases/test_one.py
+++ test_cases/test_one.py
@@ -1,17 +1,12 @@
 import pytest
 from transformation.transformation_class import TransFormClass
+
 class TestMathOperations:
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.transformer = TransFormClass()
-    
+    transformer = TransFormClass()  # Create an instance outside of any method
+
     def test_salary_one(self):
         status = self.transformer.execute_transformations()
-        assert status==True, "Status is False hence test case failed"
+        assert status == True, "Status is False hence test case failed"
 
     def test_salary_two(self):
         assert 5 - 3 == 2, "Subtraction test failed"
-    
-
-
-
--- test_cases/test_three.py
+++ test_cases/test_three.py
@@ -1,16 +1,11 @@
 import pytest
 from transformation.transformation_two import TransFormClass
-class TestMathOperations:
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.transformer = TransFormClass()
-    
+
+class TestMathOperations: 
     def test_employee_one(self):
-        status = self.transformer.execute_transformations()
-        assert status==True, "Status is False hence test case failed"
+        transformer = TransFormClass() 
+        status = transformer.execute_transformations() 
+        assert status == True, "Status is False hence test case failed"
 
     def test_employee_two(self):
         assert 5 - 3 == 2, "Subtraction test failed"
-
-
-
